c7a508f3767ed54e6ffd7108faff1c184716b342
Joey Courcelles <joey.courcelles@gmail.com>
2024-07-10 20:42:05 -0400

    Localization Work In Progress

--- SearchMaster/Tools/Localization.cs
+++ SearchMaster/Tools/Localization.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,19 @@ namespace SearchMaster.Tools
 {
     public class Localization
     {
+        private static readonly string LANGUAGES_FOLDER = "./Lang";
 
         public Localization()
+        {
+
+        }
+
+        public static Localization Load()
+        {
+            return new Localization();
+        }
+
+        public void Save()
         {
 
         }
